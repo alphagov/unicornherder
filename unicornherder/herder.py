d1678a38ecf71d3c3b729bb6e2b417105e37f5f8
Ernest W. Durbin III <ewdurbin@gmail.com>
2013-11-01 16:18:15 -0400

    one does not simply rearrange a dict

--- unicornherder/herder.py
+++ unicornherder/herder.py
@@ -13,9 +13,9 @@ log = logging.getLogger(__name__)
 COMMANDS = {
     'unicorn': 'unicorn -D -P "{pidfile}" {args}',
     'unicorn_rails': 'unicorn_rails -D -P "{pidfile}" {args}',
-    'unicorn_bin': '{unicorn_bin} -D -P "{pidfile}" {args}'
+    'unicorn_bin': '{unicorn_bin} -D -P "{pidfile}" {args}',
     'gunicorn': 'gunicorn -D -p "{pidfile}" {args}',
-    'gunicorn_django': 'gunicorn_django -D -p "{pidfile}" {args}',
+    'gunicorn_django': 'gunicorn_django -D -p "{pidfile}" {args}'
 }
 
 MANAGED_PIDS = set([])
