f9e016950c38f7a43b36b8d03a89b18542345433
Spencer Ellinor <zpencerq@users.noreply.github.com>
2014-09-10 17:12:52 -0700

    Remove unicorn_rails -P {pid} flag
    Unicorn_rails does not use the -P flag to specify the location of its pid file
    (it uses the accompanying configuration file). Unfortunately, including the -P
    flag was causing `ENV['RAILS_RELATIVE_URL_ROOT']` to be overwritten with the
    path to the unicorn pid (which is nonsensical).

--- unicornherder/herder.py
+++ unicornherder/herder.py
@@ -12,7 +12,7 @@ log = logging.getLogger(__name__)
 
 COMMANDS = {
     'unicorn': 'unicorn -D -P "{pidfile}" {args}',
-    'unicorn_rails': 'unicorn_rails -D -P "{pidfile}" {args}',
+    'unicorn_rails': 'unicorn_rails -D {args}',
     'unicorn_bin': '{unicorn_bin} -D -P "{pidfile}" {args}',
     'gunicorn': 'gunicorn -D -p "{pidfile}" {args}',
     'gunicorn_django': 'gunicorn_django -D -p "{pidfile}" {args}',
