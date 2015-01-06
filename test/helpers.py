f743ae6925dc3410b9506cf986ef4cec18e4d408
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2015-01-06 14:32:42 +0000

    Remove pesky import *
    It's more explicit to only import the things we need, and also PEP8:
    
        https://www.python.org/dev/peps/pep-0008/#imports

--- test/helpers.py
+++ test/helpers.py
@@ -1,7 +1,7 @@
 import contextlib
 
-from nose.tools import *
-from mock import *
+from nose.tools import assert_equal, assert_false, assert_raises, assert_true
+from mock import call, patch, MagicMock
 
 @contextlib.contextmanager
 def fake_timeout_fail(*args, **kwargs):
